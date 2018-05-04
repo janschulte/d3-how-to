fd140640133c46ac0bc2c063d6273e022829baa9
Jan Schulte <j.schulte@52north.org>
2018-05-04 12:57:18 +0200

    how to adjustments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { DatasetOptions, Timespan } from '@helgoland/core';
+import { D3PlotOptions } from '@helgoland/d3';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public datasetIdsMultiple = ['http://www.fluggs.de/sos2/api/v1/__63', 'http://www.fluggs.de/sos2/api/v1/__72'];
+  public colors = ['#123456', '#FF0000'];
+
+  public timespan = new Timespan(new Date().getTime() - 100000000, new Date().getTime());
+  public diagramOptionsD3: D3PlotOptions = {
+    togglePanZoom: true,
+    showReferenceValues: false,
+    generalizeAllways: true
+  };
+
+  public selectedIds: string[] = [];
+
+  public datasetOptionsMultiple: Map<string, DatasetOptions> = new Map();
+  public panZoom: any = 'zoom';
+
+  constructor(private translate: TranslateService) {
+    translate.setDefaultLang('en');
+    this.datasetIdsMultiple.forEach((entry, i) => {
+      this.datasetOptionsMultiple.set(entry, new DatasetOptions(entry, this.colors[i]));
+    });
+  }
+
+  public timespanChanged(timespan: Timespan) {
+    this.timespan = timespan;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { DatasetApiInterface, SplittedDataDatasetApiInterface } from '@helgoland/core';
+import { HelgolandD3Module } from '@helgoland/d3';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // ngx-tranlsate is used for all translations, for more informations see: http://www.ngx-translate.com/
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    HelgolandD3Module
   ],
-  providers: [],
+  providers: [
+    {
+      provide: DatasetApiInterface,
+      useClass: SplittedDataDatasetApiInterface
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
